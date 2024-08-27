f92d8b527f2b9ea7bce61a089e3393c402abe126
Allan <allancamargodeoliveira41@gmail.com>
2024-08-27 20:56:54 -0300

    tentei

--- Aulas/08-Agosto/22-08-24/script.js
+++ Aulas/08-Agosto/22-08-24/script.js
@@ -1,17 +1,16 @@
+function calculaImc() {
+  //Captura
+  let inputPeso = document.getElementById("peso");
+  let inputAltura = document.getElementById("altura");
+  let divSaida = document.getElementById("saida");
 
-function calculaImc(){ 
-    //Captura
-    let inputPeso = document.getElementById("peso")
-    let inputAltura = document.getElementById("altura")
-    let divSaida = document.getElementById("saida")
+  //Entrada
+  let peso = Number(inputPeso.value);
+  let altura = Number(inputAltura.value);
 
-    //Entrada
-    let peso = Number(inputPeso.value)
-    let altura = Number(inputAltura.value)
-    
-    //Processamento
-    let imc = peso/(altura*altura)
+  //Processamento
+  let imc = peso / (altura * altura);
 
-    //Saida
-    divSaida.innerText = imc
+  //Saida
+  divSaida.innerText = imc;
 }
--- Aulas/08-Agosto/tarefas/3-javascript atividades/ex1/script.js
+++ Aulas/08-Agosto/tarefas/3-javascript atividades/ex1/script.js
@@ -1,6 +1,18 @@
+function calcularJuros() {
+  //Captura
+  let inputCapital = document.getElementById("capital");
+  let inputTaxa = document.getElementById("taxa");
+  let inputPeriodo = document.getElementById("periodo");
+  let divSaida = document.getElementById("saida");
 
-function calcularJuros(){
+  //Entrada
+  let Capital = Number(inputCapital.value);
+  let Taxa = Number(inputTaxa.value);
+  let Periodo = Number(inputPeriodo.value);
 
-    
+  //Processamento
+  let Montante = (Capital * (1 + Taxa)) ^ Periodo;
 
+  //Saida
+  divSaida.innerText = Montante;
 }
