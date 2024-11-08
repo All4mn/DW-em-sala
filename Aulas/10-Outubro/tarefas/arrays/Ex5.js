a37f7a5a327fc65f5693002fafce53ed327440a1
Allan <allancamargodeoliveira41@gmail.com>
2024-11-07 22:01:46 -0300

    array finalizado

--- Aulas/10-Outubro/tarefas/arrays/Ex5.js
+++ Aulas/10-Outubro/tarefas/arrays/Ex5.js
@@ -2,20 +2,17 @@
 
 // Crie uma função `removeDuplicates(arr)` que remova todos os elementos duplicados do array e retorne um novo array com apenas elementos únicos.
 
-function removeDuplicates(arr){
-    let x = arr
-    let y = []
-    let j
-    for(let i = 0; i < x.length; i++){
-        j = i-1
-        if (j <= 0){ 
-            if (x[j] != x[i]){
-                y.unshift(x[j])
-            }
-        }
+function removeDuplicates(arr) {
+  let x = arr;
+  let y = [];
+  let r = [];
+  for (let i = 0; i < x.length; i++) {
+    if (!r[x[i]]) {
+      y.push(x[i]);
+      r[x[i]] = "verificado";
     }
-    return y
+  }
+  return y;
 }
-
 console.log(removeDuplicates([1, 2, 2, 3, 3, 3, 4])); // Deve exibir: [1, 2, 3, 4]
 console.log(removeDuplicates(["apple", "apple", "banana"])); // Deve exibir: ["apple", "banana"]
